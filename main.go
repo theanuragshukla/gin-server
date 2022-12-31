3ca2f30652abe43fff6a9ee3dda8032fa3b73062
theanuragshukla <tachyonanurag@gmail.com>
2022-12-31 15:00:56 +0530

    added models and routes

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+	"net/http"
+	"server/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.Use(cors.Default())
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello world")
+	})
+	// these are the endpoints
+	//C
+	router.POST("/order/create", routes.AddOrder)
+	//R
+	router.GET("/waiter/:waiter", routes.GetOrdersByWaiter)
+	router.GET("/orders", routes.GetOrders)
+	router.GET("/order/:id/", routes.GetOrderById)
+	//U
+	router.PUT("/waiter/update/:id", routes.UpdateWaiter)
+	router.PUT("/order/update/:id", routes.UpdateOrder)
+	//D
+	router.DELETE("/order/delete/:id", routes.DeleteOrder)
+
+	//this runs the server and allows it to listen to requests.
+	router.Run(":" + port)
+}
